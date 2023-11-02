5ee065493b42812cc8de25d47cd2d1668a6f2c82
Christian Jones <chjones2015@gmail.com>
2023-11-02 18:01:38 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+// Use routes
+app.use(userRoutes);
+app.use(thoughtRoutes);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
