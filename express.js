c3a4751480274bc3d6ea2a9b6a078b3372dd6275
Christian Jones <chjones2015@gmail.com>
2023-11-15 01:21:42 -0600

    first change

--- express.js
+++ express.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+const thoughtRoutes = require('./routes/thoughtRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,5 +16,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 });
 
 mongoose.set('debug', true);
+
+// Use routes
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
